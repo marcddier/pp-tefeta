4acacf08b7dde7f937de651524182f50fd84ccde
marcddier <marc.didier11@gmail.com>
2020-05-18 15:59:58 +0200

    starting new algo

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { readFileSync, readdirSync } from 'fs';
 
 const files = readdirSync('data/maps');
 
-const react1 = readFileSync('data/maps/' + files[1], 'utf-8');
+const rect1 = readFileSync('data/maps/' + files[2], 'utf-8');
 
-// console.log(react1);
+// console.log(rect1);
 
-const fileAsArray = react1.split('\n');
+const fileAsArray = rect1.split('\n');
 
 let start = 0;
 let end = 0;
@@ -25,16 +25,59 @@ fileAsArray.forEach((line, key) => {
         end = key;
       }
     });
-    // console.log(line);
   }
 });
 
 console.log(start);
 console.log(end);
 
-function resolve(startLine: number, nextLine: number) {
+function resolve(startLine: number, nextLine: number): any {
   const line = fileAsArray[startLine];
   const next = fileAsArray[nextLine];
+  let continueRight = true;
+  let continueLeft = true;
+
+  let arrayStartLine = line.split('');
+  let arrayNextLine = next.split('');
+  let startingPointIndex: number | undefined;
+  
+  if (startLine == start) {
+    startingPointIndex = arrayStartLine.findIndex(e => e == "1");
+  } else {
+    startingPointIndex = arrayStartLine.findIndex(e => e == "0");
+  }
+
+  for (let i = startingPointIndex; i < arrayStartLine.length; i ++) {
+    if (arrayStartLine[i] == "*" || arrayStartLine[i] == "\n") {
+      continueRight = false
+    } else if (arrayStartLine[i] == "2") {
+      return true
+    } else {
+      if (continueRight != false) {
+        if (arrayNextLine[i] == " ") {
+          arrayNextLine[i] = "0";
+          return resolve(startLine + 1, nextLine + 1);
+        }
+      }
+    }
+  }
+
+  for (let i = startingPointIndex; i < 9; i ) {
+    console.log("ok")
+    if (arrayStartLine[i] == "*" || arrayStartLine[i] == "\n") {
+      continueLeft = false
+    } else if (arrayStartLine[i] == "2") {
+      console.log("ok")
+      return true
+    } else {
+      if (continueLeft != false) {
+        if (arrayNextLine[i] == " ") {
+          arrayNextLine[i] = "0";
+          return resolve(startLine + 1, nextLine + 1);
+        }
+      }
+    }
+  }
 }
 
 resolve(start, start + 1);
